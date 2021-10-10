b3036f4aadf72a2ffda21134915202e18960bf9d
Tameem Alsulmi <tsalsulmi@gmail.com>
2021-10-10 21:02:30 +0300

    Solve Lab

--- main.js
+++ main.js
@@ -28,3 +28,68 @@ const characters = [
         gender: 'male',
     },
 ];
+
+
+const x = [1,2,3,4,5,6,7]
+
+
+
+
+
+
+number_exists(x , 2);
+console.log("------------------------------------------");
+specific_sum(x , 3);
+console.log("------------------------------------------");
+even_and_odd(x);
+console.log("------------------------------------------");
+getName();
+console.log("------------------------------------------");
+getheight();
+
+function number_exists(arr, key){
+    for(let i = 0 ; i< arr.length ; i++){
+if(arr[i] === key){console.log("True"); return;}
+}
+console.log("0");}
+
+
+function  specific_sum(arr, key){
+    
+    for(let i = 0 ; i< arr.length ; i++){
+      
+        for(let j = 0 ; j< arr.length ; j++){
+            if(arr[i] + arr[j] == key){console.log("Sum of "+arr[i]+"+"+arr[j]+" = "+(arr[i] + arr[j]));}
+        }
+
+
+}}
+
+
+
+function even_and_odd(arr){
+   let odd = 0 ;
+   let even = 0;
+for(let i = 0 ; i< arr.length ; i++){
+    if(arr[i]%2 === 0){even++;}
+    else{odd++;}
+}
+console.log("Numbers of even ="+even);
+console.log("Numbers of odd ="+odd);}
+
+
+
+
+
+function getName(){
+characters.map(function(element){
+    console.log(element.name);
+});
+}
+
+function getheight(){
+    characters.map(function(element){
+        console.log(element.height);
+    });
+    }
+

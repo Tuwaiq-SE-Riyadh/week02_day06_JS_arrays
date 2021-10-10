5a481c752e9ee3027fcfdd01317692f9ea6a75f1
S-MW <72529306+S-MW@users.noreply.github.com>
2021-10-11 01:53:55 +0300

    solve the lap

--- main.js
+++ main.js
@@ -1,3 +1,61 @@
+// Check if a number exists
+function checkIfANumberExists(array ,value)
+{
+    let flag = false ;
+    for(let i = 0 ; i< array.length ; i++)
+    {
+        if(array[i] === value)
+        {
+            flag = true ;
+            break
+        }
+    }
+
+    if(flag === true)
+    {
+        console.log(flag)
+    }
+    else{
+        console.log(flag)
+    }
+}
+
+// Find elements with a specific sum
+function findElementsWithASpecificSum(array,value)
+{
+    for(let i = 0 ; i< array.length ; i++)
+    {
+        for(let j = 0 ; j< array.length ; j++)
+        {
+            if(array[i] + array[j] === value)
+            console.log(array[i] + "+" + array[j] + "=" +value  )
+        }
+    }
+
+}
+
+
+// Nnumber of even and odd integers
+function numberOfEvenAndOddIntegers(array)
+{
+    let even =0 , odd = 0
+    for(let i = 0 ; i< array.length ; i++)
+    {
+        if(array[i] % 2 === 0)
+        {
+            even++
+        }
+        else{
+            odd++
+        }
+    }
+    console.log("Even :" + even + " , " + "Odd :" + odd)
+}
+
+
+//------------- (Bonus) Q -------------
+//------------- (Bonus) Q -------------
+//------------- (Bonus) Q -------------
 const characters = [
     {
         name: 'Luke Skywalker',
@@ -28,3 +86,4 @@ const characters = [
         gender: 'male',
     },
 ];
+

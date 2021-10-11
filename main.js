84a76186f85fcf0a2cf78bd026a7fa5cc7e6e792
shahad96 <shahad96.777@gmail.com>
2021-10-11 09:42:53 +0300

    task complete

--- main.js
+++ main.js
@@ -51,7 +51,7 @@ if (j === 0){
 function specificSum(arr, num){
 
     for(let i=0; i <arr.length ;i++){
-        for(let j=0; j <arr.length ;j++){
+        for(let j=i+1; j <arr.length ;j++){
             
             if(arr[i]+arr[j] === num){
                 console.log(arr[i]+"+"+arr[j]+"="+num)
